841ad7b1779fcef40174a95b34abc0b717d0e9cd
Barney <b.wilton1993@gmail.com>
2023-04-27 17:37:05 +0100

    Created HandleRotation method

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -30,5 +30,34 @@ namespace BW
             cameraObject = Camera.main.transform;
             myTransform = transform
         }
+
+        #region Movement
+        Vector3 normalVector;
+        Vector3 targetPosition;
+
+        private void HandleRotation(float delta)
+        {
+            Vector3 targetDir = Vector3.zero;
+            float moveOverride = inputHandler.moveAmount;
+            
+            targetDir = cameraObject.forward * inputHandler.vertical;
+            targetDir += cameraObject.right * inputHandler.horizontal;
+
+            targetDir.Normalize();
+            targetDir.y = 0;
+
+            if(targetDir == Vector3.zero)
+            {
+                targetDir = myTransform.forward;
+            }
+
+            float rs = rotationSpeed;
+
+            Quarternion tr = Quarternion.LookRotation(targetDir);
+            Quarternion targetRotation = Quarternion.Slerp(myTransform.rotation, tr, rs * delta);
+
+            myTransform.rotation = targetRotation;
+        }
+        #endregion
     }
 }
