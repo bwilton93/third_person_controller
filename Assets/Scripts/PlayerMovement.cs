e6c0b3d2aaa6da81f8deca554ce396a4826283d2
Barney <b.wilton1993@gmail.com>
2023-04-27 17:48:20 +0100

    set up player rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -32,6 +32,7 @@ namespace BW
             animatorHandler = GetComponentInChildren<AnimatorHandler>();
             cameraObject = Camera.main.transform;
             myTransform = transform;
+            animatorHandler.Initialize();
         }
 
         public void Update()
