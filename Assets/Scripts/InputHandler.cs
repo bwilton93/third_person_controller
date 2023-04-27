88093aeafee4dec9555d8b1ccf3ca5b942f5a5b2
Barney <b.wilton1993@gmail.com>
2023-04-27 17:25:33 +0100

    Set up OnEnable method for InputHandler.cs

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -16,5 +16,17 @@ namespace BW
 
         Vector2 movementInput;
         Vector2 cameraInput;
+
+        public void OnEnable()
+        {
+            if (inputActions == null)
+            {
+                inputActions = new PlayerControls;
+                inputActions.PlayerMovement.Movement.performed += inputActions => movementInput = inputActions.ReadValue<Vector2>();
+                inputActions.PlayerMovement.Camera.performed += i => cameraInput = i.ReadValue<Vector2>();
+            }
+        }
+
+        inputActions.Enable();
     }
 }
