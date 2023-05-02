167b92ae44ad998c3e14def392c8895fa529f407
Barney <b.wilton1993@gmail.com>
2023-05-02 16:59:05 +0100

    Created camera handler variables

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -6,6 +6,23 @@ namespace BW
 {
     public class CameraHandler : MonoBehaviour
     {
-        
+        public Transform targetTransform;
+        public Transform cameraTransform;
+        public Transform cameraPivotTransform;
+        private Transform myTransform;
+        private Vector3 cameraTransformPosition
+        private LayerMask ignoreLayers;
+
+        public static CameraHandler singleton;
+
+        public float lookSpeed = 0.1f;
+        public float followSpeed = 0.1f;
+        public float pivotSpeed = 0.03f;
+
+        private float defaultPosition;
+        private float lookAngle;
+        private float pivotAngle;
+        public float minimumPivot = -35;
+        public float maximumPivot = 35;
     }
 }
